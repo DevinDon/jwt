96514c66bc2b058848023a11b9d3c44c545d50dc
IInfinity <DevinDon@Foxmail.com>
2019-05-29 19:12:02 +0800

    init: init project

---
+++ src/main/index.ts
@@ -0,0 +1,11 @@
+import { Name } from './@types';
+
+export function hello(name: Name): string {
+  return `Hello, ${name}!`;
+}
+
+export async function asyncHello(name: Name): Promise<string> {
+  return `Hello, ${name}!`;
+}
+
+export * from './@types';
