3a500435782d8faeb68cac948d933e798b823777
IInfinity <DevinDon@Foxmail.com>
2019-05-31 01:00:10 +0800

    feat(demo): simple demo

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -1,9 +1,20 @@
-import { hello } from '../main';
-import { logger } from '@iinfinity/logger';
+import { Header, JSONWebToken, JWT, Payload } from '../main';
 
-logger.info(hello('TypeScript template'));
+const jwt = new JWT(); // same as `new JWT({ algorithm: 'HS512', key: String(Date.now()), urlsafe: false })`
+const customJWT = new JWT({ algorithm: 'HS256', key: 'key', urlsafe: true });
 
-logger.debug('debug');
-logger.error('error');
-logger.info('info');
-logger.warn('warn');
+const header: Header = {
+  alg: 'HS256',
+  typ: 'JWT'
+};
+
+const payload: Payload = {
+  username: 'user',
+  password: 'pass'
+};
+
+const token = jwt.sign(payload);
+const token256 = jwt.sign(payload, header); // use custom header
+
+const verified: JSONWebToken | undefined = jwt.verify(token); // if failed to verify, return undefined
+const verified256: JSONWebToken | undefined = jwt.verify(token); // use header.alg as Algorithm
