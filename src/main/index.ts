48a98f11cdc66a272b5cc6d35ec393df6965e010
IInfinity <DevinDon@Foxmail.com>
2019-05-31 00:59:38 +0800

    feat(index): export module

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,11 +1,6 @@
-import { Name } from './@types';
-
-export function hello(name: Name): string {
-  return `Hello, ${name}!`;
-}
-
-export async function asyncHello(name: Name): Promise<string> {
-  return `Hello, ${name}!`;
-}
+import { JWT } from './JWT';
 
 export * from './@types';
+export * from './JWT';
+
+export default JWT;
